9cb37bb94f1d14098d8fbce06f8ee8c64fc28b87
Neves0210 <gabriel@up4tech.com.br>
2023-07-14 16:58:29 -0300

    feat: fundo Gradient

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,30 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Home() {
+  return (
+    <LinearGradient 
+    colors={["#292A4E", "#715C77", "#C75C2E"]}
+    style={styles.container}> 
+        <View style={styles.content}>
+            <View style={styles.header}>
+                <View style={styles.headerLeft}>
+                    <Text>Indaiatuba</Text>
+                </View>
+            </View>
+        </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    paddingTop: 40,
+    paddingHorizontal: 35,
+  },
+  header: {},
+  headerLeft:{},
+});
